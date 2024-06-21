00ab85f0d80a5d13c9c2f8433b1f069716e8136c
Serhii Potapov <spotapov@enmark.com>
2024-06-21 23:25:59 +0200

    was added junit report in playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -17,8 +17,8 @@ module.exports = defineConfig({
     timeout: 40000,
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
-    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
+    // /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    // reporter: 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
@@ -55,33 +55,10 @@ module.exports = defineConfig({
         //   name: 'webkit',
         //   use: { ...devices['Desktop Safari'] },
         // },
-
-        /* Test against mobile viewports. */
-        // {
-        //   name: 'Mobile Chrome',
-        //   use: { ...devices['Pixel 5'] },
-        // },
-        // {
-        //   name: 'Mobile Safari',
-        //   use: { ...devices['iPhone 12'] },
-        // },
-
-        /* Test against branded browsers. */
-        // {
-        //   name: 'Microsoft Edge',
-        //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-        // },
-        // {
-        //   name: 'Google Chrome',
-        //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-        // },
     ],
-
-    /* Run your local dev server before starting the tests */
-    // webServer: {
-    //   command: 'npm run start',
-    //   url: 'http://127.0.0.1:3000',
-    //   reuseExistingServer: !process.env.CI,
-    // },
+    reporter: [
+        ['list'], // Display results in the console
+        ['junit', { outputFile: 'test-results/results.xml' }], // Generate JUnit XML report
+    ],
 });
 
