7b8511f8f389c091a6b08cc59fb7e030d8b06549
Sergey Potapov <sergeypotapoff12@gmail.com>
2025-11-19 22:12:49 +0100

    was updated yml configuration

--- global-teardown.js
+++ global-teardown.js
@@ -3,9 +3,8 @@ import { sendSlackMessage } from './slack.js';
 
 export default async function globalTeardown() {
     try {
-        // Ссылка на текущий билд
-        const buildUrl = process.env.CIRCLE_BUILD_NUM
-            ? `https://app.circleci.com/pipelines/github/${process.env.CIRCLE_PROJECT_USERNAME}/${process.env.CIRCLE_PROJECT_REPONAME}/${process.env.CIRCLE_BUILD_NUM}`
+        const buildUrl = process.env.CIRCLE_PIPELINE_NUMBER
+            ? `https://app.circleci.com/pipelines/github/${process.env.CIRCLE_PROJECT_USERNAME}/${process.env.CIRCLE_PROJECT_REPONAME}/${process.env.CIRCLE_PIPELINE_NUMBER}`
             : 'Build URL not available';
 
         const message = `✅ Tests finished! 🎉\nCircleCI Job: ${buildUrl}`;
