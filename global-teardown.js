105677df106d8a23c2517bc2913bd23d4bfcf3d0
Sergey Potapov <sergeypotapoff12@gmail.com>
2025-11-19 20:09:42 +0100

    was updated yml configuration

--- global-teardown.js
+++ global-teardown.js
@@ -1,6 +1,9 @@
-// global-teardown.js
 import { sendSlackMessage } from './slack.js';
 
 export default async function globalTeardown() {
-    await sendSlackMessage('Tests finished! 🎉');
+    const reportUrl = process.env.CIRCLE_BUILD_NUM
+        ? `https://app.circleci.com/pipelines/${process.env.CIRCLE_PROJECT_USERNAME}/${process.env.CIRCLE_PROJECT_REPONAME}/${process.env.CIRCLE_BUILD_NUM}/artifacts/0/playwright-report/index.html`
+        : 'HTML report not available';
+
+    await sendSlackMessage(`Tests finished! 🎉\nHTML report: ${reportUrl}`);
 }
