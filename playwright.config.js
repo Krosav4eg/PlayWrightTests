42e5d44159f41ff41895a9b7d46f5c4ec4c19e0f
Sergey Potapov <sergeypotapoff12@gmail.com>
2025-11-22 17:34:23 +0100

    yml config changed slack notifier status

--- playwright.config.js
+++ playwright.config.js
@@ -21,10 +21,10 @@ export default defineConfig({
             headless: false,
             use: { ...devices['Desktop Chrome'] },
         },
-        {
-            name: 'firefox',
-            use: { ...devices['Desktop Firefox'] },
-        },
+        // {
+        //     name: 'firefox',
+        //     use: { ...devices['Desktop Firefox'] },
+        // },
     ],
     reporter: [
         ['list'],
